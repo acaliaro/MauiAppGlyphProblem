696f65bb85a710ec6eb09072c8db41bba9ac7044
Alessandro Caliaro <alessandro.caliaro@soluzione1.it>
2023-01-13 08:41:31 +0100

    add font file add other samples

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -15,6 +15,7 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                fonts.AddFont("FontAwesomeSolid900.otf", "FontAwesome");
                 fonts.AddFont("fa-solid-900.ttf", "FASolid900");
             });
 
